81c510883edc9f1e1cc439d2b44418b64ca3d34b
Drakain <daniel.lindholm999@yahoo.com>
2022-10-11 15:56:43 +0000

    Fix some errors in script.js that made the buttons unresponsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,15 @@ function game() {
 
     // Function to play the game
     function playGame() {
-        const rockBtn = document.querySelector('button-rock');
-        const paperBtn = document.querySelector('button-paper');
-        const scissorsBtn = document.querySelector('button-scissors');
-        const humanBtn = document.querySelector('button-human');
-        const gunBtn = document.querySelector('button-gun');
-        const spongeBtn = document.querySelector('button-sponge');
-        const airBtn = document.querySelector('button-air');
-        const fireBtn = document.querySelector('button-fire');
-        const waterBtn = document.querySelector('button-water');
+        const rockBtn = document.querySelector('.button-rock');
+        const paperBtn = document.querySelector('.button-paper');
+        const scissorsBtn = document.querySelector('.button-scissors');
+        const humanBtn = document.querySelector('.button-human');
+        const gunBtn = document.querySelector('.button-gun');
+        const spongeBtn = document.querySelector('.button-sponge');
+        const airBtn = document.querySelector('.button-air');
+        const fireBtn = document.querySelector('.button-fire');
+        const waterBtn = document.querySelector('.button-water');
         const playerOptions = [rockBtn, paperBtn, scissorsBtn, humanBtn, gunBtn, spongeBtn, airBtn, fireBtn, waterBtn];
         const computerOptions = ['rock', 'paper', 'scissors', 'human', 'gun', 'sponge', 'air', 'fire', 'water'];
 
@@ -24,7 +24,7 @@ function game() {
         playerOptions.forEach(option => {
             option.addEventListener('click', function() {
 
-                const movesLeft = document.querySelector('moves-left');
+                const movesLeft = document.querySelector('.moves-left');
                 moves++;
                 movesLeft.innerText = `Moves left: ${10-moves}`;
 
@@ -50,77 +50,77 @@ function game() {
         player = player.toLowerCase();
         computer = computer.toLowerCase();
 
-        if (player === computer) {
+        if (player == computer) {
             result.textContent = 'Tie';
-        } else if (player === 'rock' && computer === 'fire' || 'scissors' || 'human' || 'sponge') {
+        } else if (player == 'rock' && computer == 'fire' || 'scissors' || 'human' || 'sponge') {
             result.textContent = 'You won!';
             playerScore++;
             playerScoreCount.textContent = playerScore;
-        } else if (player === 'rock' && computer === 'paper' || 'air' || 'water' || 'gun') {
+        } else if (player == 'rock' && computer == 'paper' || 'air' || 'water' || 'gun') {
             result.textContent = 'Computer won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'paper' && computer === 'rock' || 'air' || 'water' || 'gun') {
+        } else if (player == 'paper' && computer == 'rock' || 'air' || 'water' || 'gun') {
             result.textContent = 'You won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'paper' && computer === 'fire' || 'scissors' || 'human' || 'sponge') {
+        } else if (player == 'paper' && computer == 'fire' || 'scissors' || 'human' || 'sponge') {
             result.textContent = 'Computer won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'scissors' && computer === 'paper' || 'air' || 'human' || 'sponge') {
+        } else if (player == 'scissors' && computer == 'paper' || 'air' || 'human' || 'sponge') {
             result.textContent = 'You won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'scissors' && computer === 'rock' || 'fire' || 'water' || 'gun') {
+        } else if (player == 'scissors' && computer == 'rock' || 'fire' || 'water' || 'gun') {
             result.textContent = 'Computer won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'human' && computer === 'paper' || 'air' || 'water' || 'sponge') {
+        } else if (player == 'human' && computer == 'paper' || 'air' || 'water' || 'sponge') {
             result.textContent = 'You won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'human' && computer === 'rock' || 'fire' || 'scissors' || 'gun') {
+        } else if (player == 'human' && computer == 'rock' || 'fire' || 'scissors' || 'gun') {
             result.textContent = 'Computer won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'gun' && computer === 'rock' || 'fire' || 'scissors' || 'human') {
+        } else if (player == 'gun' && computer == 'rock' || 'fire' || 'scissors' || 'human') {
             result.textContent = 'You won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'gun' && computer === 'paper' || 'air' || 'water' || 'sponge') {
+        } else if (player == 'gun' && computer == 'paper' || 'air' || 'water' || 'sponge') {
             result.textContent = 'Computer won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'sponge' && computer === 'paper' || 'air' || 'water' || 'gun') {
+        } else if (player == 'sponge' && computer == 'paper' || 'air' || 'water' || 'gun') {
             result.textContent = 'You won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'sponge' && computer === 'rock' || 'fire' || 'scissors' || 'human') {
+        } else if (player == 'sponge' && computer == 'rock' || 'fire' || 'scissors' || 'human') {
             result.textContent = 'Computer won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'air' && computer === 'fire' || 'rock' || 'water' || 'gun') {
+        } else if (player == 'air' && computer == 'fire' || 'rock' || 'water' || 'gun') {
             result.textContent = 'You won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'air' && computer === 'paper' || 'scissors' || 'human' || 'sponge') {
+        } else if (player == 'air' && computer == 'paper' || 'scissors' || 'human' || 'sponge') {
             result.textContent = 'Computer won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'fire' && computer === 'scissors' || 'human' || 'paper' || 'sponge') {
+        } else if (player == 'fire' && computer == 'scissors' || 'human' || 'paper' || 'sponge') {
             result.textContent = 'You won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'fire' && computer === 'rock' || 'air' || 'water' || 'gun') {
+        } else if (player == 'fire' && computer == 'rock' || 'air' || 'water' || 'gun') {
             result.textContent = 'Computer won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'water' && computer === 'rock' || 'fire' || 'scissors' || 'gun') {
+        } else if (player == 'water' && computer == 'rock' || 'fire' || 'scissors' || 'gun') {
             result.textContent = 'You won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
-        } else if (player === 'water' && computer === 'paper' || 'air' || 'human' || 'sponge') {
+        } else if (player == 'water' && computer == 'paper' || 'air' || 'human' || 'sponge') {
             result.textContent = 'Computer won!';
             computerScore++;
             computerScoreCount.textContent = computerScore;
